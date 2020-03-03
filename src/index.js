5dccb0ff2e6762a7281222f9f207569f0b388912
Lucas Gustavo <lucas.gustavoc@live.com>
2020-03-03 17:07:13 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css'
+import 'simple-sass-gridsystem/grid.min.css'
+
+const App = props => (
+    <div>
+        <p>It's me again...</p>
+    </div>
+)
+
+ReactDOM.render(<App/>, document.getElementById('app'))
